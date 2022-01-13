9719f6d03afc765f1e7c06c3b18408a397f424d6
Alan <tsdchaos@gmail.com>
2022-01-12 20:54:20 -0500

    first commit

---
+++ src/test/java/utils/CommonMethods.java
@@ -0,0 +1,38 @@
+package utils;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+
+import java.util.concurrent.TimeUnit;
+
+public class CommonMethods {
+
+    public static WebDriver driver;
+
+
+    public static void openBrowserAndLaunchApplication(){
+        ConfigReader.readProperties(Constants.CONFIGURATION_FILEPATH);
+        switch (ConfigReader.getPropertyValue("browser")){
+            case "chrome":
+                System.setProperty("webdriver.chrome.driver", "src/Driver/chromedriver.exe");
+                driver = new ChromeDriver();
+                break;
+            case "firefox":
+                System.setProperty("webdriver.gecko.driver", "src/Driver/geckodriver.exe");
+                driver = new FirefoxDriver();
+                break;
+            default:
+                throw new RuntimeException("Invalid browser name");
+        }
+
+        driver.manage().window().maximize();
+        driver.get(ConfigReader.getPropertyValue("url"));
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+    }
+    
+    public static void closeBrowser(){
+        driver.quit();
+    }
+}
